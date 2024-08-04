17bf24ca0a144a806d30fab863d75674c5497cfb
srinivaschaligeri1 <srinivaschaligeri1@gmail.com>
2024-08-04 02:25:57 +0000

    configured Router to food recipe app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {Routes, Route, Link} from "react-router-dom"
+import NavBar from "./components/nav-bar/NavBar.jsx"
+import Home from "./pages/home/Home.jsx"
+import Favorites from "./pages/favorites/Favorites.jsx"
+import Details from  "./pages/details/Details.jsx"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+          <NavBar/>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/favorites' element={<Favorites/>}/>
+            <Route path='/recipe-item/:id' element={<Details/>}/>
+          </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </div>
   )
 }
 
